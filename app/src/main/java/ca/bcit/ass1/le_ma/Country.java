86ade649114e77d75d8fec3a889537723b737e97
MarkBCIT <dfs4mark@gmail.com>
2018-10-12 13:39:21 -0700

    sd

--- app/src/main/java/ca/bcit/ass1/le_ma/Country.java
+++ app/src/main/java/ca/bcit/ass1/le_ma/Country.java
@@ -1,4 +1,75 @@
 package ca.bcit.ass1.le_ma;
 
+
 public class Country {
+    private String Name;
+
+    private String Capital;
+
+    private String Region;
+
+    private String Population;
+
+    private String Area;
+
+    private String Borders;
+
+    private String Picture;
+
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getCapital() {
+        return Capital;
+    }
+
+    public void setCapital(String capital) {
+        Capital = capital;
+    }
+
+    public String getRegion() {
+        return Region;
+    }
+
+    public void setRegion(String region) {
+        Region = region;
+    }
+
+    public String getPopulation() {
+        return Population;
+    }
+
+    public void setPopulation(String population) {
+        Population = population;
+    }
+
+    public String getArea() {
+        return Area;
+    }
+
+    public void setArea(String area) {
+        Area = area;
+    }
+
+    public String getBorders() {
+        return Borders;
+    }
+
+    public void setBorders(String borders) {
+        Borders = borders;
+    }
+
+    public String getPicture() {
+        return Picture;
+    }
+
+    public void setPicture(String picture) {
+        Picture = picture;
+    }
 }
