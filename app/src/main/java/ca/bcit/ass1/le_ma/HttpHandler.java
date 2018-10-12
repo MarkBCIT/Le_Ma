8f1c34460d94a563321c3c146e640024c7ef9abc
MarkBCIT <dfs4mark@gmail.com>
2018-10-12 12:19:44 -0700

    sd

---
+++ app/src/main/java/ca/bcit/ass1/le_ma/HttpHandler.java
@@ -0,0 +1,4 @@
+package ca.bcit.ass1.le_ma;
+
+public class HttpHandler {
+}
--- app/src/main/java/ca/bcit/ass1/le_ma/MainActivity.java
+++ app/src/main/java/ca/bcit/ass1/le_ma/MainActivity.java
@@ -1,13 +1,143 @@
 package ca.bcit.ass1.le_ma;
 
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private String TAG = MainActivity.class.getSimpleName();
+    private ProgressDialog pDialog;
+    private ListView lv;
+    // URL to get contacts JSON
+    private static String SERVICE_URL = "https://restcountries.eu/rest/v2/all/";
+    private ArrayList<Country> countryList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        countryList = new ArrayList<Country>();
+        lv = (ListView) findViewById(R.id.countryList);
+        new GetContacts().execute();
+
     }
+
+    /**
+     * Async task class to get json by making HTTP call
+     */
+    private class GetContacts extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Showing progress dialog
+            pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setMessage("Please wait...");
+            pDialog.setCancelable(false);
+            pDialog.show();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            HttpHandler sh = new HttpHandler();
+
+            // Making a request to url and getting response
+            String jsonStr = sh.makeServiceCall(SERVICE_URL);
+
+            Log.e(TAG, "Response from url: " + jsonStr);
+
+            if (jsonStr != null) {
+                try {
+                    //JSONObject jsonObj = new JSONObject(jsonStr);
+
+                    // Getting JSON Array node
+                    JSONArray toonJsonArray = new JSONArray(jsonStr);
+
+                    // looping through All Contacts
+                    for (int i = 0; i < toonJsonArray.length(); i++) {
+                        JSONObject c = toonJsonArray.getJSONObject(i);
+
+                        String name = c.getString("Name");
+                        String capital = c.getString("Capital");
+                        String region = c.getString("Region");
+                        String population = c.getString("Population");
+                        String area = c.getString("Area");
+                        String borders = c.getString("Borders");
+                        String flag = c.getString("Flag");
+
+                        // tmp hash map for single contact
+                        Country country = new Country();
+
+                        // adding each child node to HashMap key => value
+                        country.setName(name);
+                        country.setCapital(capital);
+                        country.setRegion(region);
+                        country.setPopulation(population);
+                        country.setArea(area);
+                        country.setBorders(borders);
+                        country.setFlag(flag);
+
+                        // adding contact to contact list
+                        countryList.add(country);
+                    }
+                } catch (final JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    });
+
+                }
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
+
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+
+            // Dismiss the progress dialog
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+
+            //Toon[] toonArray = toonList.toArray(new Toon[toonList.size()]);
+
+            CountryAdapter adapter = new CountryAdapter(MainActivity.this, countryList);
+
+            // Attach the adapter to a ListView
+            lv.setAdapter(adapter);
+        }
+    }
+
 }
