ac96902d3f8b6003587e987f93914ecd776dfc06
andrewle.bc <andrewle.bc@gmail.com>
2018-10-19 14:45:53 -0700

    copy to clipboard works

--- app/src/main/java/ca/bcit/ass1/le_ma/InfoRequestActivity.java
+++ app/src/main/java/ca/bcit/ass1/le_ma/InfoRequestActivity.java
@@ -1,14 +1,42 @@
 package ca.bcit.ass1.le_ma;
 
+import android.content.Intent;
 import android.os.Build;
 import android.provider.Settings;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class InfoRequestActivity extends AppCompatActivity {
+    private ShareActionProvider shareActionProvider;
+
+    private void setShareActionIntent(String text) {
+        Intent inr = new Intent(Intent.ACTION_SEND);
+        inr.setType("text/plain");
+        inr.putExtra(Intent.EXTRA_TEXT, text);
+        shareActionProvider.setShareIntent(inr);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu. This adds items to the app bar.
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        TextView serial = findViewById(R.id.serial);
+        setShareActionIntent(serial.getText().toString());
+
+        return super.onCreateOptionsMenu(menu);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
