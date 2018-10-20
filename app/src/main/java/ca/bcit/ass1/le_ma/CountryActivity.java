0535270d40cf732dc15c11d3a5dd1d7da2f703df
andrewle.bc <andrewle.bc@gmail.com>
2018-10-19 18:25:32 -0700

    errm flag is annoying to style :(

--- app/src/main/java/ca/bcit/ass1/le_ma/CountryActivity.java
+++ app/src/main/java/ca/bcit/ass1/le_ma/CountryActivity.java
@@ -1,12 +1,41 @@
 package ca.bcit.ass1.le_ma;
 
+import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.widget.TextView;
 
 public class CountryActivity extends AppCompatActivity {
+    private ShareActionProvider shareActionProvider;
+
+    private void setShareActionIntent(String text) {
+        Intent inr = new Intent(Intent.ACTION_SEND);
+        inr.setType("text/plain");
+        inr.putExtra(Intent.EXTRA_TEXT, text);
+        shareActionProvider.setShareIntent(inr);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu. This adds items to the app bar.
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        TextView name = findViewById(R.id.Name);
+        setShareActionIntent(name.getText().toString());
+
+        return super.onCreateOptionsMenu(menu);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +47,9 @@ public class CountryActivity extends AppCompatActivity {
         String continent = Country.LIST_CONTINENTS[continentIndex];
         Country country = Country.continents.get(continent).get(countryIndex);
 
-        //DownloadImageTask dit = new DownloadImageTask(_context, imgOnePhoto);
-        //dit.execute(toon.getPicture());
-
         TextView name = findViewById(R.id.Name);
-        String nameText = name.getText() + " " + country.getName();
+        String countryName = country.getName();
+        String nameText = name.getText() + " " + countryName;
         name.setText(nameText);
 
         TextView capital = findViewById(R.id.Capital);
@@ -53,5 +80,8 @@ public class CountryActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ActionBar ab = getSupportActionBar();
+        ab.setTitle(countryName);
     }
 }
